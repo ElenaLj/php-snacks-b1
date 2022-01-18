0de0a33c9653ca47db471b8fe0d011d29d2ab81b
ElenaLj <elenaljubijankic@gmail.com>
2022-01-18 14:55:59 +0100

    snack 1

--- snack-1/index.php
+++ snack-1/index.php
@@ -6,4 +6,57 @@ Creiamo un array contenente le partite di basket di un’ipotetica tappa del cal
 Olimpia Milano - Cantù | 55-60
 */
 
+// creo array multidimensionale delle partite
+$partite = [
+    // creo array singola partita
+    [
+        "squadraCasa" => "Pallacanestro Brescia",
+        "puntiCasa" => "98",
+        "squadraOspite" => "Pallacanestro Pavia",
+        "puntiOspite" => "88"
+    ],
+    [
+        "squadraCasa" => "Basket Club Ferrara",
+        "puntiCasa" => "68",
+        "squadraOspite" => "Stella Azzurra Roma",
+        "puntiOspite" => "70"
+    ],
+    [
+        "squadraCasa" => "Pallacanestro Treviso",
+        "puntiCasa" => "93",
+        "squadraOspite" => "Victoria Libertas Pesaro",
+        "puntiOspite" => "76"
+    ],
+    [
+        "squadraCasa" => "Pallacanestro Varese",
+        "puntiCasa" => "87",
+        "squadraOspite" => "Fortitudo Pallacanestro Bologna",
+        "puntiOspite" => "74"
+    ]
+];
+
+//verifico contenuto della struttura dati
+echo "<pre> STRUTTURA DEI DATI <br>";
+var_dump($partite);
+echo "</pre>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack Pallacanestro</title>
+</head>
+<body>
+    <h2>Risultati delle partite di pallacanestro:</h2>
+    <ul>
+        <?php
+            for($i = 0; $i < count($partite); $i++){
+                echo "<li>" . $partite[$i]["squadraCasa"] . " " . " - " . $partite[$i]["squadraOspite"] . " | " . $partite[$i]["puntiCasa"] . " - " . $partite[$i]["puntiOspite"];
+            }
+        ?>
+    </ul>
+</body>
+</html>
